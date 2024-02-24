2026fc9000a20365b43db4bc4cd245e04f605a4f
Peter Chaplin <peterchaplin@btinternet.com>
2024-02-24 08:27:44 +0800

    240224

--- Utilities/Duplicate counts.py
+++ Utilities/Duplicate counts.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
-directory = 'E:/rapid7/'
-file1 = 'ukdomaintest.csv'
+directory = 'C:/domains-monitor/'
+file1 = 'mx_status.csv'
 
-df = pd.read_csv(directory + file1, encoding="ISO-8859-1", low_memory=False)
+df = pd.read_csv(directory + file1, encoding="utf-8", low_memory=False)
 print(df.shape)
+print(df.columns)
 
-df.drop_duplicates(subset='domain', keep='last',
+df.drop_duplicates(subset='mx_domain', keep='last',
                    inplace=True)
-df.sort_values(by='domain', inplace=True)
+df.sort_values(by='mx_domain', inplace=True)
 print(df.shape)
-df.to_parquet(directory + 'mxlookup.parquet', index=False)
+#df.to_parquet(directory + 'mxlookup.parquet', index=False)
