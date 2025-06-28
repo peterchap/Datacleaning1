0f7f7bad2ef242323866f2cc36bb284f8727da0d
Peter Chaplin <peterchaplin@btinternet.com>
2025-06-28 06:11:24 +0100

    280625

--- dataclean2025.py
+++ dataclean2025.py
@@ -62,11 +62,11 @@ def flag_bad_names(data: pl.DataFrame) -> pl.DataFrame:
 
     data = data.with_columns(
         pl.when(
-            (pl.col('email').str.contains(patternDel))#|
+            (pl.col('username').str.contains(patternDel))#|
             #(pl.col('FIRSTNAME').str.contains(patternDel)) |
             #(pl.col('LASTNAME').str.contains(patternDel))
         )
-        .then(pl.lit('Profanity or Role Address'))
+        .then(pl.lit('Remove'))
         .otherwise(pl.col('data_flag'))  # Keep existing value if 'data flag' exists, otherwise None
         .alias('data_flag')
     )
@@ -88,32 +88,34 @@ print("Original count: ", df.shape)
 # Remove invalid email formats
 df = flag_invalid_emails(df)
 
-df = flag_bad_names(df)
-print("Flagged emails", df.group_by("data_flag").agg(pl.count()).sort("data_flag"))
-df = df.filter(df["data_flag"].is_null())
-print("Removed invalid emails", df.shape)
 
-
-# Remove duplicates
-df = df.unique(subset=["email"])
-print("Removed duplicates", df.shape)
-
-#df.write_csv(directory + "cleaned_emails.csv", has_header=True, separator=",", quote_char='"', null_value="NULL")
-
-# Split email into left and domain parts
 df1 = df.with_columns(
     pl.col("email")
     .str.split_exact("@", 1)
     .struct.rename_fields(["username", "domain"])
     .alias("split_email")
 ).unnest("split_email")
+df1.write_parquet(directory + "split_email.parquet", compression="snappy")
+
+df1 = flag_bad_names(df1)
+print("Flagged emails", df1.group_by("data_flag").agg(pl.len()).sort("data_flag"))
+#df = df.filter(df["data_flag"].is_null())
+#print("Removed invalid emails", df.shape)
+# Remove duplicates
+df1 = df1.unique(subset=["email"])
+print("Removed duplicates", df.shape)
+
+#df.write_csv(directory + "cleaned_emails.csv", has_header=True, separator=",", quote_char='"', null_value="NULL")
+
+# Split email into left and domain parts
+
 
 print(df1.head(5))
 print("Split email into left and domain parts")
-''''
+
 domains = df1["domain"].unique()
 print("Unique domains: ", domains.shape[0])
-
+''''
 with open(directory + 'cleaning_domains.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerows([[domain] for domain in domains.to_list()])
@@ -196,31 +198,179 @@ write_path = "C:/Users/PeterChaplin/Downloads/"
 results_df.to_csv(write_path + "matched_domains.csv", index=False)
 print("Matched domains saved to CSV")
 '''
+#Process the matched domains
 domains_df = pl.read_csv(directory + "matched_domains.csv", has_header=True,
-                 separator =",", encoding= "latin", columns = ["domain", "tld_country", "is_phishing", "is_malware", "is_disposable", "is_spf_block",
-        "is_parked", "is_new_domain", "decision_flag",
-], low_memory=False)
-
+                 separator =",", encoding= "latin", columns = ["domain", "tld_country", "is_mailable",  "is_phishing", "is_malware", "is_disposable", "is_spf_block",
+                "is_parked", "is_new_domain", "decision_flag",], low_memory=False)
+#columns = ["domain", "tld_country", "is_phishing", "is_malware", "is_disposable", "is_spf_block",
+#        "is_parked", "is_new_domain", "decision_flag",
+#],
 print("Matched domains loaded from CSV")
-print("Matched domains shape: ", domains_df.shape)  
+print("Matched domains shape: ", domains_df.shape)
+domains_df.write_parquet(directory + "matched_domains.parquet", compression="snappy")
+print("Null Decision flag: ", domains_df.filter(pl.col("decision_flag").is_null()).shape[0])
+print("Null Decision flag: ", domains_df.filter(pl.col("decision_flag").is_null()).group_by("domain").agg(pl.len()).sort("domain"))  
+#process retry and dormant domains
+df_retry = pl.read_parquet(directory + "gcs_awbatch_0.parquet", 
+            columns=["domain", "tld_country", "is_mailable", "is_phishing", 
+                     "is_malware", "is_disposable", "is_spf_block", "is_parked",
+                       "is_new_domain", "decision_flag"]).select(
+            pl.col("domain"),
+            pl.col("tld_country"),
+            pl.col("is_mailable"),
+            pl.col("is_disposable"),
+            pl.col("is_phishing"),
+            pl.col("is_malware"),
+            pl.col("is_spf_block"),
+            pl.col("is_parked"),
+            pl.col("is_new_domain"),
+            pl.col("decision_flag"),
+            pl.lit(0).alias("is_dormant").cast(pl.Boolean))
 
-df_bad = domains_df.filter(pl.col("decision_flag") == 0)
-df_good = domains_df.filter(pl.col("decision_flag") == 1)
-print("Domains Good: ", df_good.shape)
-print("Domains Bad: ", df_bad.shape)
-df_final = df1.join(domains_df, on="domain", how="left")
-'''
+#print("Retry columns: ", df_retry.columns)
+print("df_retry: ", df_retry.shape)
+#print("domains_df columns", domains_df.columns)
+
+df_dormant1 = pl.read_parquet(directory + "failed_retry3_aw_missing.parquet",columns = ["domain"]).with_columns(
+    pl.lit("None").alias("tld_country").cast(pl.String),
+    pl.lit(0).alias("is_mailable").cast(pl.Boolean),
+    pl.lit(0).alias("is_disposable").cast(pl.Boolean),
+    pl.lit(0).alias("is_phishing").cast(pl.Boolean),
+    pl.lit(0).alias("is_malware").cast(pl.Boolean),
+    pl.lit(0).alias("is_spf_block").cast(pl.Boolean),
+    pl.lit(0).alias("is_parked").cast(pl.Boolean),
+    pl.lit(0).alias("is_new_domain").cast(pl.Boolean),
+    pl.lit(0).alias("decision_flag").cast(pl.Boolean),
+    pl.lit(1).alias("is_dormant").cast(pl.Boolean))
+
+df_dormant2 = pl.read_parquet(directory + "failed_retry4_aw_missing.parquet",columns = ["domain"]).with_columns(
+    pl.lit("None").alias("tld_country").cast(pl.String),
+    pl.lit(0).alias("is_mailable").cast(pl.Boolean),
+    pl.lit(0).alias("is_disposable").cast(pl.Boolean),
+    pl.lit(0).alias("is_phishing").cast(pl.Boolean),
+    pl.lit(0).alias("is_malware").cast(pl.Boolean),
+    pl.lit(0).alias("is_spf_block").cast(pl.Boolean),
+    pl.lit(0).alias("is_parked").cast(pl.Boolean),
+    pl.lit(0).alias("is_new_domain").cast(pl.Boolean),
+    pl.lit(0).alias("decision_flag").cast(pl.Boolean),
+    pl.lit(1).alias("is_dormant").cast(pl.Boolean))
+
+df_dormant = pl.concat([df_dormant1, df_dormant2]).unique(subset=["domain"])
+print("Dormant shape: ", df_dormant.shape)
+
+print("Dormant count: ", df_dormant.filter(pl.col("is_dormant") == 1).shape)
+print("Dormant columns: ", df_dormant.columns)
+
+# Ensure both DataFrames have the same schema and column names
+# Ensure both DataFrames have the same schema and column names
+
+df_retry = df_retry.with_columns(
+    pl.col("is_mailable").cast(pl.Boolean),
+    pl.col("is_phishing").cast(pl.Boolean),
+    pl.col("is_malware").cast(pl.Boolean),
+    pl.col("is_disposable").cast(pl.Boolean),
+    pl.col("is_spf_block").cast(pl.Boolean),
+    pl.col("is_parked").cast(pl.Boolean),
+    pl.col("is_new_domain").cast(pl.Boolean),
+    pl.col("decision_flag").cast(pl.Boolean),
+    pl.lit(0).alias("is_dormant").cast(pl.Boolean))
+
+domains_df = domains_df.with_columns(
+    pl.col("is_mailable").cast(pl.Boolean),
+    pl.col("is_phishing").cast(pl.Boolean),
+    pl.col("is_malware").cast(pl.Boolean),
+    pl.col("is_disposable").cast(pl.Boolean),
+    pl.col("is_spf_block").cast(pl.Boolean),
+    pl.col("is_parked").cast(pl.Boolean),
+    pl.col("is_new_domain").cast(pl.Boolean),
+    pl.col("decision_flag").cast(pl.Boolean),
+    pl.lit(0).alias("is_dormant").cast(pl.Boolean))
+
+print("Retry columns: ", df_retry.columns)
+print("Retry shape: ", df_retry.shape)
+print("Domains columns: ", domains_df.columns)
+print("Domains shape: ", domains_df.shape)
+print("Dormant columns: ", df_dormant.columns)
+print("Dormant shape: ", df_dormant.shape)
+
+# check for duplicates in the domain column
+duplicates = df_retry.filter(pl.col("domain").is_in(domains_df["domain"])).shape[0]
+print("Duplicates in df_retry: ", duplicates)
+print("Duplicates in df_dormant: ", df_dormant.filter(pl.col("domain").is_in(domains_df["domain"])).shape[0])   
+dupdormant = df_dormant.filter(pl.col("domain").is_in(domains_df["domain"])).shape[0]
+print("Duplicates in df_dormant: ", dupdormant)
+dupdomains = domains_df.filter(pl.col("domain").is_in(df_retry["domain"])).shape[0]
+print("Duplicates in df_domains: ", dupdomains)
+dupretry = df_retry.filter(pl.col("domain").is_in(df_dormant["domain"])).shape[0]
+print("Duplicates in df_retry: ", dupretry)
+#remove duplicates from domains_df
+domains_df = domains_df.filter(~pl.col("domain").is_in(df_retry["domain"]))
+print("Duplicates removed from domains_df: ", domains_df.shape[0])
+domains_df = domains_df.filter(~pl.col("domain").is_in(df_dormant["domain"]))
+print("Duplicates removed from domains_df: ", domains_df.shape[0])
+# Concatenate the DataFrames
+df_domains_all = pl.concat([df_retry, domains_df, df_dormant])
+
+print("Domains_all shape: ", df_domains_all.shape)
+#print("Domains_unique: ", df_domains_all.unique(subset=["domain"]))
+
+df_final = df1.join(df_domains_all, on="domain", how="left")
+print("Final shape: ", df_final.shape)
+
+
+missed_domains = df1.join(df_domains_all, on="domain", how="anti")
+missed_unique = missed_domains.unique(subset=["domain"])
+print("Missed unique domains: ", missed_unique.shape[0])
+
+missed_unique.write_parquet(directory + "missed_domains.parquet", compression="snappy")
+print("Missed domains saved to Parquet")
+print("Missed domains: ", missed_domains.shape[0])
+# If decision flag is null, set it to 1
 df_final = df_final.with_columns(
-    pl.when(pl.col("domain").is_null())
-    .then(pl.lit(0))
-    .otherwise(pl.lit(1))
+    pl.when(pl.col("decision_flag").is_null())
+    .then(pl.lit(1))
+    .otherwise(pl.col("decision_flag"))
     .alias("decision_flag")
 )
-'''
+# find data for traffordave.freeserve.co.uk
+print("Test: ", df_final.filter(pl.col("domain") == "traffordave.freeserve.co.uk"))
+print("Initial shape: ", df1.shape)
+print("Final columns: ", df_final.columns)
 print("Final shape: ", df_final.shape)
-print("Bad shape: ", df_final.filter(pl.col("decision_flag") == 0).shape)
-print("Good shape: ", df_final.filter(pl.col("decision_flag") == 1).shape)
+print("Bad email shape: ", df_final.filter(pl.col("decision_flag") == 0).shape)
+print("Good emails shape: ", df_final.filter(pl.col("decision_flag") == 1).shape)
+print("Bad demails: ", df_final.filter(pl.col("decision_flag") == 0).group_by("decision_flag").agg(pl.len()).sort("decision_flag"))
+print("Dormant domains: ", df_final.filter(pl.col("is_dormant") == 1).group_by("is_dormant").agg(pl.len()).sort("is_dormant"))
+print("bad dataflag", df_final.filter(pl.col("data_flag").is_not_null()).shape)
+print("bad dataflag: ", df_final.filter(pl.col("data_flag").is_not_null()).group_by("data_flag").agg(pl.len()).sort("data_flag"))
+# Ensure the columns exist in df_final before filtering
+if "decision_flag" in df_final.columns and "data_flag" in df_final.columns:
+    df_bad = df_final.filter(
+        (pl.col("decision_flag") == 0) | 
+        (pl.col("data_flag").is_not_null()) |
+        (pl.col("is_dormant") == 1))
+    df_good = df_final.filter(
+        (pl.col("decision_flag") == 1) & 
+        (pl.col("data_flag").is_null())
+        & (pl.col("is_dormant") == 0))
+    print("Emails Good: ", df_good.shape)
+    print("Emails Bad: ", df_bad.shape)
+else:
+    print("Error: Required columns 'decision_flag' or 'data_flag' are missing in df_final.")
+print("Decision FLag Breakdown: ", df_final.group_by("decision_flag").agg(pl.len()).sort("decision_flag"))
+print("Dormant flag: ", df_final.filter(pl.col("is_dormant") == 1).shape)
+print("is_mailable flag: ", df_final.filter(pl.col("is_mailable") == 0).shape)
+print("Phishing flag: ", df_final.filter(pl.col("is_phishing") == 1).shape)
+print("Malware flag: ", df_final.filter(pl.col("is_malware") == 1).shape)
+print("Disposable flag: ", df_final.filter(pl.col("is_disposable") == 1).shape)
+print("SPF block flag: ", df_final.filter(pl.col("is_spf_block") == 1).shape)
+print("Parked flag: ", df_final.filter(pl.col("is_parked") == 1).shape)
+print("New domain flag: ", df_final.filter(pl.col("is_new_domain") == 1).shape)
+print("Decision flag: ", df_final.filter(pl.col("decision_flag") == 0).shape)
+print("Data flag: ", df_final.filter(pl.col("data_flag").is_not_null()).shape)
+print("Data flag: ", df_final.filter(pl.col("data_flag").is_not_null()).group_by("data_flag").agg(pl.len()).sort("data_flag"))
 
-#process retries and
-df_retry = pl.read_parquet(directory + "processed_aw_missing.parquet")
-df_retry2 = pl.read_parquet(directory + "processed_retry3_aw_missing.parquet")
+# Save the final DataFrame to a Parquet file
+df_final.write_parquet(directory + "cleaned_emails.parquet", compression="snappy")
+print("Cleaned emails saved to Parquet")
+# Save Bad Emails to Parquet
